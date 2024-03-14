cdad448d80578d9a2459f23ebe8e148f48519ab0
SHUBHAM KUMAR <shubham.rajveer19@gmail.com>
2024-03-14 16:21:18 +0530

    changes

--- Backend/connection/DbConnect.js
+++ Backend/connection/DbConnect.js
@@ -3,9 +3,9 @@ require("dotenv").config()
 
 const connection=sql.createConnection({
     host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database:process.env.DATABASE,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
     timezone:"Asia/Kolkata",
     port:process.env.PORT
 })
